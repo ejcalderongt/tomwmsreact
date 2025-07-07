5ea5f52d6c39b6f53cf9c6b5c5da7fa45fbafdbf
ejcalderongt <ejcalderon02@yahoo.com>
2025-07-07 19:35:31 +0000

    host

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,12 @@ export default defineConfig({
     host: "0.0.0.0",
     allowedHosts: [
       "1da7a6d0-45be-4c44-bd76-6384ef21d0f0-00-1zyv481h72tzi.riker.replit.dev",
+      "e762bf80-a710-434d-ba76-1a7a948d14af-00-2yvukxrilq4n6.riker.replit.dev",
     ],
     proxy: {
       "/api": {
         target: "http://52.41.114.122:8091",
-        changeOrigin: true,      
+        changeOrigin: true,
       },
     },
   },
