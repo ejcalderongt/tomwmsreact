73702b3d797351c4dcdef030129db47efdbfe0d5
Erik Calderon <44594929-dtsolutionsgt@users.noreply.replit.com>
2025-07-07 18:16:36 +0000

    repplit 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 5000,
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+    allowedHosts: [
+      "1da7a6d0-45be-4c44-bd76-6384ef21d0f0-00-1zyv481h72tzi.riker.replit.dev",
+    ],
+  },
+});
